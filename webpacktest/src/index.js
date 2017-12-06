74848ae770f8bae5bc3f684da0a5ae3a87a1031a
sang5052003 <sang5052003@rosesystems.kr>
2017-12-06 17:18:27 +0900

    for OutputManagement tutorial 	deleted:    src/dog.jpg 	modified:   src/index.js 	deleted:    src/style.css 	modified:   webpack.config.js

--- webpacktest/src/index.js
+++ webpacktest/src/index.js
@@ -1,18 +1,8 @@
 import _ from 'lodash'
-import './style.css'
-import DogImg from './dog.jpg'
 
 function component() {
     var element = document.createElement('div')
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-    
-    //add style
-    element.classList.add('hello')
-    
-    //add img
-    var dog = new Image()
-    dog.src = DogImg
-    element.appendChild(dog)
     
     return element
 }
