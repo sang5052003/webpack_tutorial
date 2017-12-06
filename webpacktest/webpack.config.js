e11a144830286da646a71d8a77ee047669fb0911
sang5052003 <sang5052003@rosesystems.kr>
2017-12-06 18:00:22 +0900

    html-webpack-plugin설치 webpack.config에 plugin을 추가해서 index.html이 replace되도록 변경 plugin이 entry에 들어있는 것들을 bundle시켜서 index.html에 포함시킨다 	modified:   dist/index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   webpack.config.js

--- webpacktest/webpack.config.js
+++ webpacktest/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path')
 
+//entry포인트를 추가하거나 변경해도
+//plugin이 index.html을 replace해준다(내 bundle들이 추가 되어있는)
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     /*entry: './src/index.js',
     output: {
@@ -10,6 +14,11 @@ module.exports = {
         app: './src/index.js',
         print: './src/print.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
